309349d4ad68b4bc8f98a5e8ec732cba995aaf5b
Twan Luttik <twanluttik@gmail.com>
2020-01-17 05:51:10 -0800

    x

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  margin: 0 auto;
+
+  @media only screen and (min-width: 700px) {
+    max-width: 700px;
+  }
 }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+* {
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ src/screens/home/homeScreen.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="homeScreen">
+    homescreen
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+#homeScreen {
+  background: red;
+  height: 100%;
+  width: 100%;
+}
+</style>
