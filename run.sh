85a13de82687b1fda9cc61b4d184e4dfd79b5a1a
Twan Luttik <twanluttik@gmail.com>
2020-01-17 19:01:04 -0800

    x

---
+++ run.sh
@@ -0,0 +1,17 @@
+# remove dist folder
+rm -rf ./dist/
+
+# Install node modules
+npm i
+
+# Run build
+npm run build
+
+# Remove the current ....
+rm -rf /var/www/notify-stats/
+
+# move the dist folder to ....
+mv -a ./dist/ /var/www/notify-stats/
+
+
+echo done
