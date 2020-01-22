aa9a99b8a1d4559e96d103788f0495f6979fea39
Twan Luttik <twanluttik@gmail.com>
2020-01-22 00:59:47 -0800

    x

--- src/core/core.js
+++ src/core/core.js
@@ -5,21 +5,24 @@ import stats from './collections/stats.collection';
 import admin from './collections/admin.collection';
 
 Pulse.use(Vue);
+console.log(process.env)
 
 export default new Pulse({
   collections: {
     stats,
     admin
   },
+  persist: ['apis'],
   request: {
-    baseURL: process.env.PROD ? 'https://api.twanluttik.com' : 'http://localhost:3005',
+    baseURL: process.env.NODE_ENV !== 'development' ? 'https://api.twanluttik.com' : 'http://localhost:3005',
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Authorization': '7b286cd1-8354-4bf7-a9b7-9ec30a2f18da',
       Origin: 'https://track.notify.services/',
       Dev: false,
     }
+  },
+  data: {
+    apis: ['https://api.twanluttik.com', 'http://localhost:3005']
   }
 });
-// https://api.twanluttik.com
-// http://localhost:3005
