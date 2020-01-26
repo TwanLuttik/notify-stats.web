794543cbd209a23cf025ecc7505c3d49c7c355c2
Twan Luttik <twanluttik@gmail.com>
2020-01-26 04:00:19 -0800

    removed unused code

--- src/screens/channel/parts/trackList.vue
+++ src/screens/channel/parts/trackList.vue
@@ -13,18 +13,12 @@
       <div class="segment">
         <p>{{ currentChannel.track[i].subs }}</p>
         <p v-if="currentChannel.track[i+1]" class="diff" :style="{ color: (calcDifference(currentChannel.track[i+1].subs, currentChannel.track[i].subs) > 0 ? '#56a147' : 'RED')  }" >{{ test(currentChannel.track[i+1].subs, currentChannel.track[i].subs) }}{{ calcDifference(currentChannel.track[i+1].subs, currentChannel.track[i].subs) }}</p>
-        <!-- <p> {{ ((calcDifference(currentChannel.track[i+1].subs, currentChannel.track[i].subs) > 0) ? '+' : '-') }}</p> -->
       </div>
 
       <div class="segment">
         <p>{{ currentChannel.track[i].posts }}</p>
         <p v-if="currentChannel.track[i+1]" class="diff" :style="{ color: (calcDifference(currentChannel.track[i+1].posts, currentChannel.track[i].posts) > 0 ? '#56a147' : 'RED')  }" >{{ test(currentChannel.track[i+1].posts, currentChannel.track[i].posts) }}{{ calcDifference(currentChannel.track[i+1].posts, currentChannel.track[i].posts) }}</p>
       </div>
-      <!-- :style="{ color: (calcDifference(currentChannel.track[i+1].subs, currentChannel.track[i].subs) > 0 ? '#56a147' : 'RED')  }" -->
-      <!-- <p class="segment">{{ currentChannel.track[i].posts }}  -->
-        <!-- {{ calcDifference(currentChannel.track[i].posts, currentChannel.track[(i - 1)].posts) > 0? '+' : '' }} -->
-        <!-- {{ calcDifference(currentChannel.track[i].posts, currentChannel.track[(i - 1)].posts) }} -->
-      <!-- </p> -->
       <p class="segment">{{ currentChannel.track[i].month }}</p>
     </div>
     
