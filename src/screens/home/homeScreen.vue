07f0df859422d9b4ae1fe15d43a7a41d5ed8d90c
Twan Luttik <twanluttik@gmail.com>
2020-02-02 16:37:01 -0800

    change pos

--- src/screens/home/homeScreen.vue
+++ src/screens/home/homeScreen.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="homeScreen">
-    <h2>Welcome to Notify statistics</h2>
-
+    <h2 style="font-size:25px;">Welcome to First Notify statistics site</h2>
+    
+    <br>
+    <br>
+    <br>
+    
      <img class="image" src="@/assets/images/notify-artwork-1.jpg" />
      <div class="gradiant"/>
      <div class="author">
@@ -9,27 +13,21 @@
        <p class="link" @click="$root.to('https://twitter.com/noroiju')">@noroiju</p>
      </div>
 
-
-
-    <div style="display:flex;flex-directin:column;justify-content:center;">
-      <p>Made By</p><p class="link" style="padding-left:5px;" @click="to('https://twitter.com/TwanLuttik')">@TwanLuttik</p>
-    </div>
-
-    
     <TopChannels/>
 
     <br>
     <br>
     <br>
+    <br>
+    <br>
 
+     <div style="display:flex;flex-directin:column;justify-content:center;">
+      <p>Made By</p><p class="link" style="padding-left:5px;" @click="to('https://twitter.com/TwanLuttik')">@TwanLuttik</p>
+    </div>
     <div style="display:flex;flex-directin:column;justify-content:center;margin:30px 0;">
       <p>If you see any issues send me a dm on</p>
       <p class="link" style="padding-left:5px;" @click="to('https://twitter.com/TwanLuttik')">twitter</p>
     </div> 
-
-    <!-- <div v-for="(item, i) in top" :key="i">
-      {{ item }}
-    </div> -->
 
   </div>
 </template>
